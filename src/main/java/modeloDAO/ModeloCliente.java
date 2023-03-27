1bcdd852cd50ce526a1a86a726d2af24c313a658
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-03-27 12:46:49 +0200

    metodo modificarCliente

--- src/main/java/modeloDAO/ModeloCliente.java
+++ src/main/java/modeloDAO/ModeloCliente.java
@@ -45,6 +45,34 @@ public class ModeloCliente extends Conector{
 			
 			pst.execute();
 			return true;
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+	
+	public boolean modificarCliente(Cliente cliente) {
+		
+		String st = "UPDATE clientes SET nombre = ?, apellido = ?, dni = ?, email = ?, contrasena = ?, telefono = ?, fecha_nacimiento = ? WHERE id = ?";
+		
+		try {
+			PreparedStatement pst = super.connection.prepareStatement(st);
+			
+			pst.setString(1, cliente.getNombre());
+			pst.setString(2, cliente.getApellido());
+			pst.setString(3, cliente.getDni());
+			pst.setString(4, cliente.getEmail());
+			pst.setString(5, cliente.getContrasena());
+			pst.setInt(6, cliente.getTelefono());
+			pst.setDate(7, new Date(cliente.getFechaNacimiento().getTime()));
+			pst.setInt(8, cliente.getId());
+
+			pst.execute();
+			return true;
+			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
