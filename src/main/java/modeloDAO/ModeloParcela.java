4920956a5132d96828c664e653abded8af4ef3bc
Alejandro <ikcev@plaiaundi.net>
2023-03-27 13:30:23 +0200

    Arreglar getZona y getTipo

---
+++ src/main/java/modeloDAO/ModeloParcela.java
@@ -0,0 +1,35 @@
+package modeloDAO;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import modeloDTO.Parcela;
+import modeloDTO.Zona;
+
+public class ModeloParcela extends Conector {
+	public boolean insertarZona(Parcela parcela) {
+
+		String st = "INSERT INTO parcelas (zona,tipo,grupo,limpia,cantidad_max,cantidad_min) VALUES (?,?,?,?,?,?)";
+
+		try {
+			PreparedStatement pst = super.connection.prepareStatement(st);
+
+			pst.setString(1, new Zona(parcela.getZona()));
+			pst.setString(2, parcela.getTipo());
+			pst.setInt(3, parcela.getGrupo());
+			pst.setBoolean(4, parcela.getLimpia());
+			pst.setInt(5, parcela.getCantidadMax());
+			pst.setInt(6, parcela.getCantidadMin());
+
+			pst.execute();
+			return true;
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return false;
+	}
+
+}
