2b944ac73589fef11d144bd9b2885d419c7a1848
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-03-27 13:06:26 +0200

    setter y getters

--- src/main/java/modeloDTO/Mascota.java
+++ src/main/java/modeloDTO/Mascota.java
@@ -5,4 +5,29 @@ public class Mascota {
 	private String nombre;
 	private int numChip;
 	private String raza;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getNumChip() {
+		return numChip;
+	}
+	public void setNumChip(int numChip) {
+		this.numChip = numChip;
+	}
+	public String getRaza() {
+		return raza;
+	}
+	public void setRaza(String raza) {
+		this.raza = raza;
+	}
 }
