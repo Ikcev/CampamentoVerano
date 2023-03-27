b7938736a44a325bcfb48ffdea302b314a6dbe56
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-03-27 13:15:11 +0200

    metodo insertarUsuario

--- src/main/java/modeloDAO/ModeloMascota.java
+++ src/main/java/modeloDAO/ModeloMascota.java
@@ -1,5 +1,30 @@
 package modeloDAO;
 
-public class ModeloMascota {
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import modeloDTO.Mascota;
+
+public class ModeloMascota extends Conector{
+	public boolean insertarMascota(Mascota mascota) {
+		
+		String st = "INSERT INTO mascotas (nombre, num_chip, raza) VALUES (? ?, ?)";
+		
+		try {
+			PreparedStatement pst = super.connection.prepareStatement(st);
+			
+			pst.setString(1, mascota.getNombre());
+			pst.setInt(2, mascota.getNumChip());
+			pst.setString(3, mascota.getRaza());
+			
+			pst.execute();
+			return true;
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
 }
