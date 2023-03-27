eb721a92a6d774b6bd86c53c5a455fbe4420ac76
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-03-27 12:16:56 +0200

    atributos definidos

--- src/main/java/modeloDTO/Cliente.java
+++ src/main/java/modeloDTO/Cliente.java
@@ -1,5 +1,14 @@
 package modeloDTO;
 
-public class Cliente {
+import java.util.Date;
 
+public class Cliente {
+	private int id;
+	private String nombre;
+	private String apellido;
+	private String dni;
+	private String email;
+	private String contrasena = "1234";
+	private int telefono;
+	private Date fechaNacimiento;
 }
