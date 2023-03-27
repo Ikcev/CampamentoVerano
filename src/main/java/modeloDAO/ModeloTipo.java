e1925a218f4a361cd55acff2597ce0295e7e90a3
Alejandro <ikcev@plaiaundi.net>
2023-03-27 13:37:19 +0200

    Insertar Tipo

---
+++ src/main/java/modeloDAO/ModeloTipo.java
@@ -0,0 +1,30 @@
+package modeloDAO;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import modeloDTO.Tipo;
+
+public class ModeloTipo extends Conector {
+	public boolean insertarZona(Tipo tipo) {
+
+		String st = "INSERT INTO tipos (nombre,cantidad_personas,descripcion) VALUES (?,?,?)";
+
+		try {
+			PreparedStatement pst = super.connection.prepareStatement(st);
+
+			pst.setString(1, tipo.getNombre());
+			pst.setInt(2, tipo.getCantidad_personas());
+			pst.setString(3, tipo.getDescripcion());
+
+			pst.execute();
+			return true;
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return false;
+	}
+}
