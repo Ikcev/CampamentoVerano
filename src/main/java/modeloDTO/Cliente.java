56ae826bbf698e5101fdbc39719fdd3ea55804ab
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-04-17 10:36:25 +0200

    ahora extends Usuario y tiene arraylist de mascotas

--- src/main/java/modeloDTO/Cliente.java
+++ src/main/java/modeloDTO/Cliente.java
@@ -1,65 +1,15 @@
 package modeloDTO;
 
-import java.util.Date;
+import java.util.ArrayList;
 
-public class Cliente {
-	private int id;
-	private String nombre;
-	private String apellido;
-	private String dni;
-	private String email;
-	private String contrasena = "1234";
-	private int telefono;
-	private Date fechaNacimiento;
-	
-	public int getId() {
-		return id;
+public class Cliente extends Usuario{
+	private ArrayList<Mascota> mascotas = new ArrayList<>();
+
+	public ArrayList<Mascota> getMascotas() {
+		return mascotas;
 	}
-	public void setId(int id) {
-		this.id = id;
+
+	public void agregarMascota(Mascota mascota) {
+		mascotas.add(mascota);
 	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public String getApellido() {
-		return apellido;
-	}
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
-	}
-	public String getDni() {
-		return dni;
-	}
-	public void setDni(String dni) {
-		this.dni = dni;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getContrasena() {
-		return contrasena;
-	}
-	public void setContrasena(String contrasena) {
-		this.contrasena = contrasena;
-	}
-	public int getTelefono() {
-		return telefono;
-	}
-	public void setTelefono(int telefono) {
-		this.telefono = telefono;
-	}
-	public Date getFechaNacimiento() {
-		return fechaNacimiento;
-	}
-	public void setFechaNacimiento(Date fechaNacimiento) {
-		this.fechaNacimiento = fechaNacimiento;
-	}
-	
-	
 }
