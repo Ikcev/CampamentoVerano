a06b0fb4fe1984a23e1e804a9e443f3de56b6e27
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-04-17 10:35:56 +0200

    extends Usuario

--- src/main/java/modeloDTO/Personal.java
+++ src/main/java/modeloDTO/Personal.java
@@ -1,16 +1,5 @@
 package modeloDTO;
 
-import java.util.Date;
-
-public abstract class Personal {
-	protected Integer id;
-	protected String dni;
-	protected String nombre;
-	protected String apellido;
-	protected Date fecha_nacimiento;
-	protected Integer telefono;
-	protected String email;
-	protected Date fecha_ingreso;
-	protected String contrasena;
-	protected String manager;
+public abstract class Personal extends Usuario{
+	
 }
