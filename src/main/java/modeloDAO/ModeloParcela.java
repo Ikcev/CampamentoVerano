394d961d5b2cb1f699ce4543fb7d80d94f07daa5
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-04-18 09:36:11 +0200

    metodos insertar y eliminar

---
+++ src/main/java/modeloDAO/ModeloParcela.java
@@ -0,0 +1,49 @@
+package modeloDAO;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import modeloDTO.Parcela;
+
+public class ModeloParcela extends Conector{
+	public boolean insertarParcela(Parcela parcela) {
+		String st = "INSERT INTO parcelas (id_tipo, id_zona, id_grupo, limpia) values ?,?,?,?";
+		
+		try {
+			PreparedStatement pst = super.connection.prepareStatement(st);
+			
+			pst.setInt(1, parcela.getTipo().getId());
+			pst.setInt(2, parcela.getZona().getId());
+			pst.setInt(3, parcela.getGrupo().getId());
+			pst.setBoolean(4, parcela.getLimpia());
+			
+			pst.execute();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+	
+	public boolean eliminarZona(int id) {
+		String st = "DELETE FROM parcelas WHERE id = ?";
+		
+		try {
+			PreparedStatement pst = super.connection.prepareStatement(st);
+			
+			pst.setInt(1, id);
+			
+			pst.execute();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+	
+	
+}
