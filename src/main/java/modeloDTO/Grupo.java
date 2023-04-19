60876c1087b6fcf87562d5c2cb2adca403277645
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-04-19 13:18:30 +0200

    tipos de dato corregidos

--- src/main/java/modeloDTO/Grupo.java
+++ src/main/java/modeloDTO/Grupo.java
@@ -3,15 +3,14 @@ package modeloDTO;
 import java.util.ArrayList;
 
 public class Grupo {
-	private Integer id;
+	private int id;
 	private Monitor monitor;
-	private Parcela parcela;
 	private ArrayList<Cliente> clientes;
 	
-	public Integer getId() {
+	public int getId() {
 		return id;
 	}
-	public void setId(Integer id) {
+	public void setId(int id) {
 		this.id = id;
 	}
 	public Monitor getMonitor() {
@@ -20,17 +19,10 @@ public class Grupo {
 	public void setMonitor(Monitor monitor) {
 		this.monitor = monitor;
 	}
-	public Parcela getParcela() {
-		return parcela;
-	}
-	public void setParcela(Parcela parcela) {
-		this.parcela = parcela;
-	}
 	public ArrayList<Cliente> getClientes() {
 		return clientes;
 	}
 	public void setClientes(ArrayList<Cliente> clientes) {
 		this.clientes = clientes;
 	}
-	
 }
