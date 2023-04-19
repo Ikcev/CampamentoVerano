e57870a2e57ee722d01eb6a322fde632262cc7ef
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-04-19 13:19:41 +0200

    ahora cliente tiene grupo

--- src/main/java/modeloDTO/Cliente.java
+++ src/main/java/modeloDTO/Cliente.java
@@ -4,6 +4,15 @@ import java.util.ArrayList;
 
 public class Cliente extends Usuario{
 	private ArrayList<Mascota> mascotas = new ArrayList<>();
+	private Grupo grupo;
+
+	public Grupo getGrupo() {
+		return grupo;
+	}
+
+	public void setGrupo(Grupo grupo) {
+		this.grupo = grupo;
+	}
 
 	public ArrayList<Mascota> getMascotas() {
 		return mascotas;
