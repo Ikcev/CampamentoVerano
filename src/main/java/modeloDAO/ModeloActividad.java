b72f38513e6a471ec9d06510f5f1bae68e003511
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-04-19 13:42:52 +0200

    metodo eliminarActividad

--- src/main/java/modeloDAO/ModeloActividad.java
+++ src/main/java/modeloDAO/ModeloActividad.java
@@ -1,5 +1,47 @@
 package modeloDAO;
 
-public class ModeloActividad extends Conector{
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import modeloDTO.Actividad;
+
+public class ModeloActividad extends Conector{
+	public boolean insertarActividad(Actividad actividad) {
+		String st = "INSERT INTO actividades (id_zona, nombre, cantidad_max, edad_min) VALUES ?,?,?,?";
+		
+		try {
+			PreparedStatement pst = super.connection.prepareStatement(st);
+			
+			pst.setInt(1, actividad.getZona().getId());
+			pst.setString(2, actividad.getNombre());
+			pst.setInt(3, actividad.getCantidad_max());
+			pst.setInt(4, actividad.getEdad_min());
+			
+			pst.execute();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+	
+	public boolean eliminarActividad(int id) {
+		String st = "DELETE FROM actividades WHERE id=?";
+		
+		try {
+			PreparedStatement pst = super.connection.prepareStatement(st);
+			
+			pst.setInt(1, id);
+			
+			pst.execute();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
 }
