b998f7ea2f8bc5db58cc53edd1df1ecfea458197
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-04-19 13:00:33 +0200

    metodo modificarZona

--- src/main/java/modeloDAO/ModeloZona.java
+++ src/main/java/modeloDAO/ModeloZona.java
@@ -41,4 +41,22 @@ public class ModeloZona extends Conector{
 		
 		return false;
 	}
+	
+	public boolean modificarZona(Zona zona) {
+		String st = "UPDATE zonas SET nombre=? WHERE id=?";
+		
+		try {
+			PreparedStatement pst = super.connection.prepareStatement(st);
+			
+			pst.setString(1, zona.getNombre());
+			
+			pst.execute();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
 }
