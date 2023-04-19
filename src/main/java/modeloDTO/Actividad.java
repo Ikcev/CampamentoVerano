7bed539393aa5e714383135d14044f56734a06cf
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-04-19 13:21:27 +0200

    tipos de dato corregidos

--- src/main/java/modeloDTO/Actividad.java
+++ src/main/java/modeloDTO/Actividad.java
@@ -1,16 +1,16 @@
 package modeloDTO;
 
 public class Actividad {
-	private Integer id;
+	private int id;
 	private Zona zona;
 	private String nombre;
-	private Integer cantidad_max;
-	private Integer edad_min;
+	private int cantidad_max;
+	private int edad_min;
 	
-	public Integer getId() {
+	public int getId() {
 		return id;
 	}
-	public void setId(Integer id) {
+	public void setId(int id) {
 		this.id = id;
 	}
 	public Zona getZona() {
@@ -25,16 +25,16 @@ public class Actividad {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	public Integer getCantidad_max() {
+	public int getCantidad_max() {
 		return cantidad_max;
 	}
-	public void setCantidad_max(Integer cantidad_max) {
+	public void setCantidad_max(int cantidad_max) {
 		this.cantidad_max = cantidad_max;
 	}
-	public Integer getEdad_min() {
+	public int getEdad_min() {
 		return edad_min;
 	}
-	public void setEdad_min(Integer edad_min) {
+	public void setEdad_min(int edad_min) {
 		this.edad_min = edad_min;
 	}
 	
