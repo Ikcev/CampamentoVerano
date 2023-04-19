3154b114c12e4ec7e9a4dee2788c8d83db0c80fa
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-04-19 08:26:55 +0200

    metodo eliminarUsuario

--- src/main/java/modeloDAO/ModeloTipo.java
+++ src/main/java/modeloDAO/ModeloTipo.java
@@ -25,4 +25,22 @@ public class ModeloTipo extends Conector{
 		
 		return false;
 	}
+	
+	public boolean eliminarTipo(int id) {
+		String st = "DELETE FROM tipos WHERE id = ?";
+		
+		try {
+			PreparedStatement pst = super.connection.prepareStatement(st);
+			
+			pst.setInt(1, id);
+			
+			pst.execute();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
 }
