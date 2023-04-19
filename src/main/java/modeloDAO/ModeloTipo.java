cb356ddeeaf4940820d5425bfc83aefd7085b6f6
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-04-19 08:23:59 +0200

    metodo insertarTipo

--- src/main/java/modeloDAO/ModeloTipo.java
+++ src/main/java/modeloDAO/ModeloTipo.java
@@ -1,5 +1,28 @@
 package modeloDAO;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import modeloDTO.Tipo;
+
 public class ModeloTipo extends Conector{
-	
+	public boolean insertarTipo(Tipo tipo) {
+		String st = "INSERT INTO tipos (nombre, cantidad_personas, descripcion) VALUES ?, ? ,?";
+		
+		try {
+			PreparedStatement pst = super.connection.prepareStatement(st);
+			
+			pst.setString(1, tipo.getNombre());
+			pst.setInt(2, tipo.getCantidad_personas());
+			pst.setString(3, tipo.getDescripcion());
+			
+			pst.execute();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
 }
