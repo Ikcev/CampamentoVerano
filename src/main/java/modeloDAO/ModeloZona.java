18bbf4329467a90a6539f1df4a14df12e90d5d66
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-04-19 09:13:39 +0200

    metodo insertarZona

--- src/main/java/modeloDAO/ModeloZona.java
+++ src/main/java/modeloDAO/ModeloZona.java
@@ -1,5 +1,26 @@
 package modeloDAO;
 
-public class ModeloZona extends Conector{
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import modeloDTO.Zona;
+
+public class ModeloZona extends Conector{
+	public boolean insertarZona(Zona zona) {
+		String st = "INSERT INTO zonas(nombre) VALUES ?";
+		
+		try {
+			PreparedStatement pst = super.connection.prepareStatement(st);
+			
+			pst.setString(1, zona.getNombre());
+			
+			pst.execute();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
 }
