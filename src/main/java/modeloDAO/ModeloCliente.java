2424a8dfbdb38629a2f089c2c5457fa87930eb51
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-04-24 09:43:24 +0200

    metodo eliminarcliente

--- src/main/java/modeloDAO/ModeloCliente.java
+++ src/main/java/modeloDAO/ModeloCliente.java
@@ -34,4 +34,21 @@ public class ModeloCliente extends Conector{
 		return false;
 	}
 
+	public boolean eliminarCliente(int id) {
+		String st = "DELETE FROM clientes WHERE id=?";
+		
+		try {
+			PreparedStatement pst = super.connection.prepareStatement(st);
+			
+			pst.setInt(1, id);
+			
+			pst.execute();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
 }
