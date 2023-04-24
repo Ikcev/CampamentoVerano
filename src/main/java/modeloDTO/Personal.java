79144dd2d7c6e759bb342e33cb1f6dfd2889581e
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-04-24 10:26:54 +0200

    el personal ahora tiene fecha de ingresp

--- src/main/java/modeloDTO/Personal.java
+++ src/main/java/modeloDTO/Personal.java
@@ -1,7 +1,18 @@
 package modeloDTO;
 
+import java.util.Date;
+
 public class Personal extends Usuario{
 	private int director;
+	private Date fechaIngreso;
+
+	public Date getFechaIngreso() {
+		return fechaIngreso;
+	}
+
+	public void setFechaIngreso(Date fechaIngreso) {
+		this.fechaIngreso = fechaIngreso;
+	}
 
 	public int getDirector() {
 		return director;
