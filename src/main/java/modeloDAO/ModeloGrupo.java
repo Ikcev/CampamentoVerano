5cf8c9a77070df06224c6cdc8e1387802db3e27a
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-04-24 09:20:26 +0200

    metodo insertarGrupo

--- src/main/java/modeloDAO/ModeloGrupo.java
+++ src/main/java/modeloDAO/ModeloGrupo.java
@@ -1,5 +1,26 @@
 package modeloDAO;
 
-public class ModeloGrupo {
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import modeloDTO.Grupo;
+
+public class ModeloGrupo extends Conector{
+	public boolean insertarGrupo(Grupo grupo) {
+		String st = "INSERT INTO grupos (id_monitor) VALUES ?";
+		
+		try {
+			PreparedStatement pst = super.connection.prepareStatement(st);
+			
+			pst.setInt(1, grupo.getMonitor().getId());
+			
+			pst.execute();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
 }
