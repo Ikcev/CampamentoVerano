8684ca6fc61121e490fedadf0c9d7c7e084de0f1
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-04-24 12:05:56 +0200

    metodo insertar

--- src/main/java/modeloDAO/ModeloActividadesGrupo.java
+++ src/main/java/modeloDAO/ModeloActividadesGrupo.java
@@ -1,5 +1,30 @@
 package modeloDAO;
 
-public class ModeloActividadesGrupo {
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import modeloDTO.ActividadesPorGrupo;
+
+public class ModeloActividadesGrupo extends Conector{
+	public boolean insertarActividadGrupo(ActividadesPorGrupo actividadesPorGrupo) {
+		String st = "INSERT INTO actividades_grupo VALUES ?,?,?,?";
+		
+		try {
+			PreparedStatement pst = super.connection.prepareStatement(st);
+			
+			pst.setInt(1, actividadesPorGrupo.getActividad().getId());
+			pst.setInt(2, actividadesPorGrupo.getGrupo().getId());
+			pst.setDate(3, new Date(actividadesPorGrupo.getFecha().getTime()));
+			pst.setString(4, actividadesPorGrupo.getHora());
+			
+			pst.execute();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
 }
