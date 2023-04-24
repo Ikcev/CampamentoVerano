f69887d831aa778a884471cafe96e11df2f25bba
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-04-24 10:30:33 +0200

    metodo insertar

--- src/main/java/modeloDAO/ModeloPersonal.java
+++ src/main/java/modeloDAO/ModeloPersonal.java
@@ -1,5 +1,29 @@
 package modeloDAO;
 
-public class ModeloPersonal {
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import modeloDTO.Personal;
+
+public class ModeloPersonal extends Conector{
+	public boolean insertarPersonal(Personal personal) {
+		String st = "INSERT INTO personal VALUES ?,?,?";
+		
+		try {
+			PreparedStatement pst = super.connection.prepareStatement(st);
+			
+			pst.setInt(1, personal.getId());
+			pst.setDate(2, new Date(personal.getFechaIngreso().getTime()));
+			pst.setInt(3, personal.getDirector());
+			
+			pst.execute();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
 }
