1aaa161806bee33b072899cbe724b33742ff947f
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-04-24 09:41:11 +0200

    insertarCliente

---
+++ src/main/java/modeloDAO/ModeloCliente.java
@@ -0,0 +1,37 @@
+package modeloDAO;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import modeloDTO.Cliente;
+
+public class ModeloCliente extends Conector{
+//	private Cliente clienteHeredaUsuario(int id) {
+//		ModeloUsuario modeloUsuario = new ModeloUsuario();
+//		modeloUsuario.conectar();
+//		
+//		Cliente cliente = (Cliente) modeloUsuario.getUsuarios(id);
+//		
+//		return cliente;
+//	}
+	
+	public boolean insertarCliente(Cliente cliente) {
+		String st = "INSERT INTO clientes VALUES ?,?";
+		
+		try {
+			PreparedStatement pst = super.connection.prepareStatement(st);
+			
+			pst.setInt(1, cliente.getId());
+			pst.setInt(2, cliente.getGrupo().getId());
+			
+			pst.execute();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+
+}
