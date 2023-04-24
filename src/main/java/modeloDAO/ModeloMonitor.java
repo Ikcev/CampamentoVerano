ea70500e0b2e9f3d7208cd77cd5be9a544b0b2ad
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-04-24 08:22:48 +0200

    metodo insertarMonitor

--- src/main/java/modeloDAO/ModeloMonitor.java
+++ src/main/java/modeloDAO/ModeloMonitor.java
@@ -1,5 +1,41 @@
 package modeloDAO;
 
-public class ModeloMonitor {
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import modeloDTO.Monitor;
+
+public class ModeloMonitor extends Conector{
+	private Monitor monitorHeredaUsuario(int id) {
+		ModeloUsuario modeloUsuario = new ModeloUsuario();
+		modeloUsuario.conectar();
+		
+		Monitor monitor = (Monitor) modeloUsuario.getUsuarios(id);
+		
+		return monitor;
+	}
+	
+	public boolean insertarMonitor(int id) {
+		Monitor monitor = monitorHeredaUsuario(id);
+		
+		if (monitor == null) {
+			return false;
+		}
+		
+		String st = "INSERT INTO monitor VALUES ?";
+		
+		try {
+			PreparedStatement pst = super.connection.prepareStatement(st);
+			
+			pst.setInt(1, id);
+			
+			pst.execute();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
 }
