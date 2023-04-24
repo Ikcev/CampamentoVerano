06db6b021eec6f191d1601915ed6e41250c0d2a9
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-04-24 13:49:15 +0200

    metodo que devuelve un arraylist de usuarioVehiculo

--- src/main/java/modeloDAO/ModeloUsuarioVehiculo.java
+++ src/main/java/modeloDAO/ModeloUsuarioVehiculo.java
@@ -1,5 +1,43 @@
 package modeloDAO;
 
-public class ModeloUsuarioVehiculo {
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
+import modeloDTO.UsuarioVehiculo;
+
+public class ModeloUsuarioVehiculo extends Conector{
+	public ArrayList<UsuarioVehiculo> getUsuarioVehiculoIDUsuario(int id) {
+		String st = "SELECT * FROM usuario_vehiculo WHERE id_usuario=?";
+		ArrayList<UsuarioVehiculo> usuarioVehiculos = new ArrayList<>();
+		ModeloUsuario modeloUsuario = new ModeloUsuario();
+		ModeloVehiculos modeloVehiculos = new ModeloVehiculos();
+		
+		modeloUsuario.conectar();
+		modeloVehiculos.conectar();
+		
+		try {
+			PreparedStatement pst = super.connection.prepareStatement(st);
+			
+			pst.setInt(1, id);
+			
+			ResultSet rs = pst.executeQuery();
+			while(rs.next()) {
+				UsuarioVehiculo usuarioVehiculo = new UsuarioVehiculo();
+				
+				usuarioVehiculo.setUsuario(modeloUsuario.getUsuarios(rs.getInt("id_usuario")));
+				usuarioVehiculo.setVehiculo(modeloVehiculos.getVehiculo(rs.getInt("id_vehiculo")));
+				
+				usuarioVehiculos.add(usuarioVehiculo);
+			}
+			
+			return usuarioVehiculos;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
 }
