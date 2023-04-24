3e8a548c547ee820f548b3dbcfdd54f0c6415715
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-04-24 11:35:28 +0200

    metodo insertarVehivulo

--- src/main/java/modeloDAO/ModeloVehiculos.java
+++ src/main/java/modeloDAO/ModeloVehiculos.java
@@ -1,5 +1,29 @@
 package modeloDAO;
 
-public class ModeloVehiculos {
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import modeloDTO.Vehiculo;
+
+public class ModeloVehiculos extends Conector{
+	public boolean insertarVehiculo(Vehiculo vehiculo) {
+		String st = "INSERT INTO vehiculos(matricula, marca, modelo, color) VALUES ?,?,?,?";
+		
+		try {
+			PreparedStatement pst = super.connection.prepareStatement(st);
+			
+			pst.setString(1, vehiculo.getMatricula());
+			pst.setString(2, vehiculo.getMarca());
+			pst.setString(1, vehiculo.getModelo());
+			pst.setString(4, vehiculo.getColor());
+			
+			pst.execute();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
 }
