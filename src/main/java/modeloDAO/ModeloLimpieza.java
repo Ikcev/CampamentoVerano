757a4861f513aebb179344ab46fe2bdd8c9cb504
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-04-24 08:46:56 +0200

    metodo insertarLimpieza

--- src/main/java/modeloDAO/ModeloLimpieza.java
+++ src/main/java/modeloDAO/ModeloLimpieza.java
@@ -1,5 +1,42 @@
 package modeloDAO;
 
-public class ModeloLimpieza {
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import modeloDTO.Limpieza;
+
+public class ModeloLimpieza extends Conector{
+	private Limpieza limpiezaHeredaUsuario(int id) {
+		ModeloUsuario modeloUsuario = new ModeloUsuario();
+		modeloUsuario.conectar();
+		
+		Limpieza limpieza = (Limpieza) modeloUsuario.getUsuarios(id);
+		
+		return limpieza;
+	}
+	
+	public boolean insertarLimpieza(int id) {
+		Limpieza limpieza = limpiezaHeredaUsuario(id);
+		
+		if (limpieza == null) {
+			return false;
+		}
+		
+		String st = "INSERT INTO limpieza VALUES ?,?";
+		
+		try {
+			PreparedStatement pst = super.connection.prepareStatement(st);
+			
+			pst.setInt(1, limpieza.getId());
+			pst.setInt(2, limpieza.getZona().getId());
+			
+			pst.execute();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return true;
+	}
 }
