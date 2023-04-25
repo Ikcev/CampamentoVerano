a17693795fecde6d67c97563dc1d4783426932c6
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-04-25 08:35:45 +0200

    insertar y eliminar usuario_vehiculo

--- src/main/java/modeloDAO/ModeloUsuarioVehiculo.java
+++ src/main/java/modeloDAO/ModeloUsuarioVehiculo.java
@@ -8,6 +8,44 @@ import java.util.ArrayList;
 import modeloDTO.UsuarioVehiculo;
 
 public class ModeloUsuarioVehiculo extends Conector{
+	public boolean insertarUsuarioVehiculo(UsuarioVehiculo usuarioVehiculo) {
+		String st = "INSERT INTO usuario_vehiculo VALUES ?,?";
+		
+		try {
+			PreparedStatement pst = super.connection.prepareStatement(st);
+			
+			pst.setInt(1, usuarioVehiculo.getUsuario().getId());
+			pst.setInt(2, usuarioVehiculo.getVehiculo().getId());
+			
+			pst.execute();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+	
+	public boolean eliminarUsuarioVehiculo(UsuarioVehiculo usuarioVehiculo) {
+		String st = "DELETE FROM usuario_vehiculo WHERE id_usuario=? and id_vehiculo=?";
+		
+		try {
+			PreparedStatement pst = super.connection.prepareStatement(st);
+
+			pst.setInt(1, usuarioVehiculo.getUsuario().getId());
+			pst.setInt(2, usuarioVehiculo.getVehiculo().getId());
+			
+			pst.execute();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+	
 	public ArrayList<UsuarioVehiculo> getUsuarioVehiculoIDUsuario(int id) {
 		String st = "SELECT * FROM usuario_vehiculo WHERE id_usuario=?";
 		ArrayList<UsuarioVehiculo> usuarioVehiculos = new ArrayList<>();
