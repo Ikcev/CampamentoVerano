b0f8f9cb56dd8cb01f262204908669380f37a068
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-04-25 09:19:31 +0200

    metodo eliminar reserva

--- src/main/java/modeloDAO/ModeloReserva.java
+++ src/main/java/modeloDAO/ModeloReserva.java
@@ -7,8 +7,7 @@ import java.sql.SQLException;
 import modeloDTO.Reserva;
 
 public class ModeloReserva extends Conector{
-	public boolean insertarZona(Reserva reserva) {
-
+	public boolean insertarReserva(Reserva reserva) {
 		String st = "INSERT INTO reservas (id_parcela,id_clientes,fecha_ingreso,fecha_salida) VALUES (?,?,?,?)";
 
 		try {
@@ -28,4 +27,26 @@ public class ModeloReserva extends Conector{
 
 		return false;
 	}
+	
+	public boolean eliminarReserva(Reserva reserva) {
+		String st = "DELETE FROM reservas WHERE id_parcela=? and id_clientes=? and fecha_ingreso=? and fecha_salida=?";
+		
+		try {
+			PreparedStatement pst = super.connection.prepareStatement(st);
+
+			pst.setInt(1, reserva.getParcela().getId());
+			pst.setInt(2, reserva.getCliente().getId());
+			pst.setDate(3, new Date(reserva.getFecha_ingreso().getTime()));
+			pst.setDate(4, new Date(reserva.getFecha_salida().getTime()));
+
+			pst.execute();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+	
 }
