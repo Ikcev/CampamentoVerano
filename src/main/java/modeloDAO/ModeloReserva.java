d54ba47ec887df218660bc2998e34609eebe3f19
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-04-25 09:16:47 +0200

    insert reserva

--- src/main/java/modeloDAO/ModeloReserva.java
+++ src/main/java/modeloDAO/ModeloReserva.java
@@ -1,5 +1,6 @@
 package modeloDAO;
 
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
@@ -8,16 +9,18 @@ import modeloDTO.Reserva;
 public class ModeloReserva extends Conector{
 	public boolean insertarZona(Reserva reserva) {
 
-		String st = "INSERT INTO reservas (parcela,clientes,fecha_ingreso,fecha_salida) VALUES (?,?,?,?)";
+		String st = "INSERT INTO reservas (id_parcela,id_clientes,fecha_ingreso,fecha_salida) VALUES (?,?,?,?)";
 
 		try {
 			PreparedStatement pst = super.connection.prepareStatement(st);
 
 			pst.setInt(1, reserva.getParcela().getId());
+			pst.setInt(2, reserva.getCliente().getId());
+			pst.setDate(3, new Date(reserva.getFecha_ingreso().getTime()));
+			pst.setDate(4, new Date(reserva.getFecha_salida().getTime()));
 
 			pst.execute();
 			return true;
-
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
