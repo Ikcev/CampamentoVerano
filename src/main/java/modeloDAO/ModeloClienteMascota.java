4f27f9b7a5e21d1fadcb2f5b88e574ab2eda357b
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-04-25 08:55:36 +0200

    insertar

--- src/main/java/modeloDAO/ModeloClienteMascota.java
+++ src/main/java/modeloDAO/ModeloClienteMascota.java
@@ -1,5 +1,27 @@
 package modeloDAO;
 
-public class ModeloClienteMascota {
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import modeloDTO.ClienteMascota;
+
+public class ModeloClienteMascota extends Conector{
+	public boolean insertarClienteMascota(ClienteMascota clienteMascota) {
+		String st = "INSERT INTO cliente_mascota VALUES ?, ?";
+		
+		try {
+			PreparedStatement pst = super.connection.prepareStatement(st);
+			
+			pst.setInt(1, clienteMascota.getCliente().getId());
+			pst.setInt(2, clienteMascota.getMascota().getId());
+			
+			pst.execute();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
 }
