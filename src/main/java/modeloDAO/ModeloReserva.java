bfd3e1b1e17f40cac05ea746df718534a9ec025f
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-04-25 09:28:25 +0200

    get y getAll reservas

--- src/main/java/modeloDAO/ModeloReserva.java
+++ src/main/java/modeloDAO/ModeloReserva.java
@@ -2,7 +2,9 @@ package modeloDAO;
 
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import modeloDTO.Reserva;
 
@@ -49,4 +51,65 @@ public class ModeloReserva extends Conector{
 		return false;
 	}
 	
+	public Reserva getReserva(int id_cliente) {
+		String st = "SELECT * FROM reservas WHERE id_cliente=?";
+		
+		try {
+			PreparedStatement pst = super.connection.prepareStatement(st);
+			
+			pst.setInt(1, id_cliente);
+			
+			ResultSet rs = pst.executeQuery();
+			rs.next();
+			
+			Reserva reserva = rellernarReserva(rs);
+			
+			return reserva;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+
+	private Reserva rellernarReserva(ResultSet rs) throws SQLException {
+		ModeloCliente modeloCliente = new ModeloCliente();
+		ModeloParcela modeloParcela = new ModeloParcela();
+		
+		modeloCliente.conectar();
+		modeloParcela.conectar();
+		
+		Reserva reserva = new Reserva();
+		
+		reserva.setCliente(modeloCliente.getCliente(rs.getInt("id_cliente")));
+		reserva.setParcela(modeloParcela.getParcela(rs.getInt("id_parcela")));
+		reserva.setFecha_ingreso(rs.getDate("fecha_ingreso"));
+		reserva.setFecha_salida(rs.getDate("fecha_salida"));
+		
+		modeloCliente.cerrar();
+		modeloParcela.cerrar();
+		return reserva;
+	}
+	
+	public ArrayList<Reserva> getAllReservas() {
+		String st = "SEELCT * FROM reservas";
+		ArrayList<Reserva> reservas = new ArrayList<>();
+		
+		try {
+			PreparedStatement pst = super.connection.prepareStatement(st);
+			
+			ResultSet rs = pst.executeQuery();
+			while (rs.next()) {
+				reservas.add(rellernarReserva(rs));
+			}
+			
+			return reservas;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
 }
